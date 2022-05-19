297cd90c1e97045b1dbde30f612862d76f6a23db
Paula Guastella <pauguastella@gmail.com>
2022-05-19 08:59:37 -0300

    Se corrige meta viewport en todas las páginas, se agrega h1 a todas las páginas, se cierra etiqueta form

--- form.php
+++ form.php
@@ -4,28 +4,28 @@ $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $mail = $_POST['email'];
 $telefono = $_POST['telefono'];
-$alquiler  = $_POST['alquiler'];
-$dosplantas = $_POST['dos_plantas'];
-$tipologia = $POST['tipologia'];
-$hab = $POST['hab'];
-$provincia = $POST['provincia'];
+//$alquiler  = $_POST['alquiler'];
+//$dosplantas = $_POST['dos_plantas'];
+//$tipologia = $POST['tipologia'];
+//$hab = $POST['hab'];
+//$provincia = $POST['provincia'];
 $mensaje = $POST['multi'];
 
 //Vamos a mostrar un texto plano
-$header .= "Content-Type: text/plain";
+//$header .= "Content-Type: text/plain";
 
 // Como me va a llegar el cuerpo del mail a mi
 $mensaje = "Nombre de quien consulta". $nombre .",\r\n";
-$mensaje = "Apellido de quien consulta". $apellido .",\r\n";
+$mensaje = "Apellido de quien consulta". $apellido . ",\r\n";
 $mensaje = "Su e-mail es". $mail .",\r\n";
-$mensaje = "Su número de teléfono es". $telefono .",\r\n";
-$mensaje = "¿El cliente alquila actualmente?" .$alquiler .",\r\n";
-$mensaje = "¿Al cliente le interesan las viviendas de dos plantas?" .$dosplantas .",\r\n";
-$mensaje = "Tipología favorita:" .$tipologia .",\r\n";
-$mensaje = "Cantidad de habitaciones:" .$hab .",\r\n";
-$mensaje = "Provincia de instalación:" .$provincia .",\r\n";
-$mensaje = "Mensaje:". $mensaje .",\r\n";
-$mensaje = "Enviado el" .date('d/m/Y', time());
+$mensaje = "Su número de teléfono es". $telefono . ",\r\n";
+//$mensaje = "¿El cliente alquila actualmente?" . $alquiler . ",\r\n";
+//$mensaje = "¿Al cliente le interesan las viviendas de dos plantas?" . $dosplantas . ",\r\n";
+//$mensaje = "Tipología favorita:" . $tipologia . ",\r\n";
+//$mensaje = "Cantidad de habitaciones:" . $hab . ",\r\n";
+//$mensaje = "Provincia de instalación:" . $provincia . ",\r\n";
+$mensaje = "Mensaje:". $mensaje . ",\r\n";
+$mensaje = "Enviado el" . date('d/m/Y', time());
 
 
 $destinatario = 'pauguastella@gmail.com';//defino destinatario
